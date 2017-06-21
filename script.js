cd15b58eb919dd10cf171102a4f118452e31b2ec
Hack Reactor Student <student@hackreactor.com>
2017-06-21 15:43:50 -0500

    Completed wednesday-PM lesson, choice button allows page to restart, either by pressing it or pressing enter

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 // your javascript and jQuery code goes below
   $("#pink").click(function(){
-    $("#container").css("border", "2px solid pink")
+    $("#container").css("border", "2px solid pink");
   });
   $("#blue").click(function(){
     $("#container").css("background-color", "#4286f4");
@@ -9,7 +9,39 @@ $(document).ready(function() {
   $("#fade").click(function(){
     $("#container").fadeOut(2000);
   });
-  $("lottery").click(function(){
+  $("#lottery").click(function(){
     alert("YOU WON THE LOTTERY");
   });
+  $("#confirm").click(function(){
+    confirm("Are you sure?");
+  });
+  $("#puppy").click(function(){
+    $("body").css("background-image", "url('Images/puppy.jpg')");
+  });
+  $("#replace").click(function(){
+    $("#replace").replaceWith("<h2>REPLACED!</h2>");
+  });
+  $("#drive").click(function(){
+    var age = prompt("How old are you?");
+    if (age >= 15){
+      var canDrive = $("#container").text("WOHOOO!!!");
+    } else {
+      $("#container").text("Sorry, maybe next year");
+    }
+  });
+  $("#choose").click(function(){
+    $("#container").remove();
+    clone.appendTo("body");
+    clone.attr("id", "container");
+    clone = $("#container").clone(true, true);
+  });
+  $(document).keypress(function(key) {
+    if(key.which == 13) {
+      $("#container").remove();
+      clone.appendTo("body");
+      clone.attr("id", "container");
+      clone = $("#container").clone(true, true);
+    }
+  });
+  var clone = $("#container").clone(true, true);
 })
