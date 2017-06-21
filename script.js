ca9632fe57853ea1c4e48d21bff2932ff6a39d10
Hack Reactor Student <student@hackreactor.com>
2017-06-21 14:31:40 -0500

    Started wednesday-PM lesson, completed buttons and pink and blue function layouts

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
 // your javascript and jQuery code goes below
-
+  $("#pink").click(function(){
+    $("#container").css("border", "2px solid pink")
+  });
+  $("#blue").click(function(){
+    $("#container").css("background-color", "#4286f4");
+  });
+  $("#fade").click(function(){
+    $("#container").fadeOut(2000);
+  });
+  $("lottery").click(function(){
+    alert("YOU WON THE LOTTERY");
+  });
 })
