2ee62e56f351d6ad8194cf394638128806162eff
Hack Reactor Student <student@hackreactor.com>
2017-06-21 15:16:12 -0500

    finished button

--- script.js
+++ script.js
@@ -9,13 +9,30 @@ $(document).ready(function() {
     $("#container").css("border-color", "#FF52CD")
     $("#container").css("border-spacing","20px")
   })
-
-
-
-
-
-
-
-
-
+  $("#fade").click(  function fd(){
+    $("#container").fadeOut(5000)
+  })
+  $("#lot").click(  function lt(){
+    alert("YOU WON THE LOTTERY")
+  })
+  $("#com").click(  function cm(){
+    alert("Are you sure?")
+  })
+  $("#puppy").click(  function pppy(){
+    $("#container").css("background-image", "url(images/puppy.jpg)")
+  })
+  $("#repl").click(  function rpl(){
+    $("#repl").replaceWith("REPLACED!")
+  })
+  $("#drive").click(  function drv(){
+    var pr =prompt("How olde are you?")
+    if (pr<16){
+      $("#container").replaceWith("Sorry, maybe next year")
+    }else{
+     $("#container").replaceWith("WOHOOO!!!") 
+    }
+  })
+  $("#choo").click(  function ch(){
+    $("body").css("background-image","url(https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png)")
+  })
 })
