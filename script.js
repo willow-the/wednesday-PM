0763ea0c0479a43510632d29627fc859b78320e3
Hack Reactor Student <student@hackreactor.com>
2017-06-21 14:31:39 -0500

    pink / blue buttons

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
 // your javascript and jQuery code goes below
+  
+  $("#blue").click (function blu(){
+    $("#container").css("background-color", "#52FFF7")
+  })
+
+  $("#pink").click(  function pnk(){
+    $("#container").css("border-color", "#FF52CD")
+    $("#container").css("border-spacing","20px")
+  })
+
+
+
+
+
+
+
+
 
 })
