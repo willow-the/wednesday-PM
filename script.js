0c2289b6f7a3c4f3ac01596e60cbcbb85f8125f1
Hack Reactor Student <student@hackreactor.com>
2017-06-21 15:27:53 -0500

    Crazy Buttons

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
-// your javascript and jQuery code goes below
 
+  $("#pink").click(function() {
+    $("#container").css("border", "20px solid pink");
+  })
+  $("#blue").click(function() {
+    $("#container").css("background-color", "#00ccff");
+  })
+  $("#fade").click(function() {
+    $("#container").fadeOut(2000);
+  })
+  $("#lottery").click(function() {
+    alert("You won the lottery!");
+  })
+  $("#confirm").click(function() {
+    alert("Are you sure?");
+  })
+  $("#puppy").click(function() {
+    $("#container").css("background-image", "url('images/puppy.jpg')");
+  })
+  $("#replace").click(function() {
+    $("#replace").replaceWith("REPLACED!");
+  })
 })
