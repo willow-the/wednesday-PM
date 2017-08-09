eb63a3708541fe5d0426199136f99f48421c739b
Hack Reactor Student <student@hackreactor.com>
2017-08-09 12:30:37 -0500

    yay

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 $(document).ready(function() {
-// your javascript and jQuery code goes below
+$("#purple").click(function() {
+  $("#container").css("border", "20px solid purple");
+});
+
+$('#blue').click(function() {
+  $('#container').css("background-color", "#60caff")
+});
+
+$('#fade').click(function() {
+  $('#container').fadeOut(10000)
+});
+
+$('#lottery').click(function() {
+  alert("YOU WON THE LOTTERY!!!");
+});
+
+$('#confirmation').click(function() {
+  var answer = confirmation("Are you sure...?");
+  if answer {
+    alert("GOOD!");
+  } 
+});
 
 })
